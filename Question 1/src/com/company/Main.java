32c53e91c44bb7cb4f57c80686bacdc97155a6af
jayminpatel007 <jaymin.future365@gmail.com>
2021-12-31 22:15:09 +0530

    Fixed main class name in question 1

--- Question 1/src/com/company/Main.java
+++ Question 1/src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class Main2 {
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
